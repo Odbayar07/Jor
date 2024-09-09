0a2d1317930e0969f9aec248be22a8facbeb280f
Odbayar07 <odbayar2024@gmail.com>
2024-09-09 17:35:10 +0800

    state object ehluulev

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,27 @@
 require('@babel/polyfill');
 import Search from './model/Search'
 
-let search = new Search('pasta');
+const state = {};
 
-search.doSearch().then(r => console.log(r));
+const controlSearch = async () => {
+  // webees hailtiin tulhuur gargana.
+  const query = 'pizza';
+  
+  if(query){
+    // shineer hailtiin object uusgene.
+    state.search = new Search(query);
+    // hailt hiihed zoriulj interface beltgene.
+
+    // hailtiig guitsetgene.
+    await state.search.doSearch();
+
+    // hailtiin ur dung gargaj irne.
+    console.log(state.search.result)
+
+  }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+})
